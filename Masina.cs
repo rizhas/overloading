48c0bed31326bddda147819a4a9f63d1fdcd5332
rizhas <rizhas69@gmail.com>
2018-03-15 01:16:55 +0200

    Update Masina.cs, Program.cs, and overloading.csproj Sukuriau MASIVO pagrintu mashinu kataloga. nepabaigta mintis bet veikia kartu su overloading'u.

---
+++ Masina.cs
@@ -0,0 +1,97 @@
+﻿using System;
+namespace overloading
+{
+    public class Masina
+    {
+        private string marke;
+        private string modelis;
+
+        private string[] masinuMasivas = new string[] { "audi", "vw", "bmw" };
+
+        private string[][] markiuMasivas = new string[][] { new string[]{ "200", "100", "80" }, new string[]{ "jeta", "pasat", "golf" }, new string[]{"325","530","750"} };
+
+        public Masina(string Marke, string Modelis)
+        {
+            marke = Marke;
+            modelis = Modelis;
+            if (true)
+            {
+
+            }
+            markeIRmodelis(Marke,Modelis);
+        }
+        
+        public Masina(string Modelis)
+        {
+            modelis = Modelis;
+            if (true)
+            {
+
+            }
+            kuris(Modelis);
+        }
+        
+        private void markeIRmodelis(string mar, string mod)
+        {
+            bool rado = true;
+            Console.Clear();
+            for (int i = 0; i < masinuMasivas.Length; i++)           
+            {
+                if (mar == masinuMasivas[i])
+                {
+                    //Console.WriteLine(masinuMasivas[i]);
+                    //Console.WriteLine(markiuMasivas[i].Length);
+                    rado = true;
+                    for (int e = 0;e < 3; e++)   // NELEIDO IRASHYTI VIETON 3 markiuMasivas[i].Length !!!
+                    {
+                        //Console.WriteLine(markiuMasivas[i][e]);
+                        if (mod == markiuMasivas[i][e])
+                        {
+                            rado = true;
+                            Console.WriteLine($"Jusu pasirinktas auto ({mar} {mod}) yra pardavime.");
+                            break;
+                        }
+                        else
+                        {
+                            rado = false;
+                        }
+                    }
+                }
+                
+            }
+            if (rado == false)
+            {
+                Console.WriteLine($"Siuo metu auto ({mar} {mod}) NETURIME PARDAVIME.");
+            }
+        }
+        private void kuris(string model)
+        {
+            bool rado = true;
+            int e = 0;
+            Console.Clear();
+            for (; e < 3; e++)
+            {
+                for (int i = 0; i < markiuMasivas.Length; i++)
+                {
+                    if (model != markiuMasivas[e][i])
+                    {
+                        //Console.WriteLine(markiuMasivas[e][i]);
+                        //Console.WriteLine(e);
+                        rado = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("radau");
+                        Console.WriteLine($"Jusu marke ({model}) priklauso gamintojuj ({masinuMasivas[e]}) ir mes turim ja pardavime.");
+                    }
+
+                }
+            }
+
+            if (rado == false)
+            {
+                Console.WriteLine($"Marke {model} Nerastas!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+
+namespace overloading
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            string b = "";
+            do
+            {
+                Console.WriteLine("Jus zinote norimos markes gamintoja? (t) or (n)");
+                b = Console.ReadLine();
+            } while (b!="t" && b!="n");
+
+            if (b == "t")
+            {
+                Console.Clear();
+                Console.Write("Iveskite gamintoja: ");
+                string gamintojas = Console.ReadLine();
+                Console.Write("Iveskite marke: ");
+                string marke = Console.ReadLine();
+                Masina masina = new Masina(gamintojas, marke);
+            }
+
+            if (b == "n")
+            {
+                Console.Clear();
+                Console.Write("Iveskite marke: ");
+                Masina masina2 = new Masina(Console.ReadLine());
+            }
+            
+//            Masina masina = new Masina(Console.ReadLine(), Console.ReadLine());
+            
+ //           Masina masina2 = new Masina(Console.ReadLine());
+
+        }
+    }
+}
